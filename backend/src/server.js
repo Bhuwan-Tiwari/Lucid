e098e7a56ef3aed8fa718107bdf6a40e2cf71fff
Bhuwan Tiwari <bhuwan1200@gmail.com>
2025-08-25 19:33:49 +0530

    Cors added

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,7 +27,7 @@ const limiter = rateLimit({
 });
 app.use('/api/', limiter);
 
-app.use(cors);
+app.use(cors());
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
